9ff894d87ea3a89f177c73efa47a530a07df62e3
David Friedl <david.friedl@whu.edu>
2019-05-15 16:12:02 +0200

    Yelp Fake

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.delete_all
+
+Restaurant.create!(
+  [
+    {
+      name: "The Shed",
+      address: "London",
+      category: "chinese",
+      phone_number: 0127344512,
+    },
+    {
+      name: "Portale 52",
+      address: "Karlsruhe",
+      category: "italian",
+      phone_number: 123456,
+    },
+    {
+      name: "Chupenga",
+      address: "Berlin",
+      category: "french",
+      phone_number: 456367,
+    },
+    {
+      name: "Burger King",
+      address: "Shanghai",
+      category: "chinese",
+      phone_number: 4553463,
+    },
+    {
+      name: "Random Restaurant",
+      address: "Milan",
+      category: "italian",
+      phone_number: 42311,
+    },
+  ]
+)
